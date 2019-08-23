c86f9a2bc788ff136d0b83603eab5fcd82b932bc
David Araujo <davidsdearaujo@gmail.com>
2019-08-23 01:39:03 -0300

    Adicionado paralax com a imagem do palestrante ao background

--- lib/src/home/pages/event/event_page.dart
+++ lib/src/home/pages/event/event_page.dart
@@ -13,16 +13,27 @@ class EventPage extends StatefulWidget {
 
 class _EventPageState extends State<EventPage> {
   PageController _pageController;
+  ScrollController _backgroundController;
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController(viewportFraction: 0.65);
+    _backgroundController = ScrollController();
+
+    _pageController.addListener(() {
+      var _backgroundOffset = _pageController.offset *
+          _backgroundController.position.maxScrollExtent /
+          _pageController.position.maxScrollExtent;
+          
+      _backgroundController.position.jumpTo(_backgroundOffset);
+    });
   }
 
   @override
   void dispose() {
     _pageController.dispose();
+    _backgroundController.dispose();
     super.dispose();
   }
 
@@ -31,16 +42,33 @@ class _EventPageState extends State<EventPage> {
     return Scaffold(
         body: Stack(
       children: <Widget>[
+        ListView.builder(
+          // reverse: true,
+          scrollDirection: Axis.horizontal,
+          controller: _backgroundController,
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Image.network(
+              index.isEven
+                  ? "https://i2.wp.com/flutterando.com.br/wp-content/uploads/2019/06/3305e16d-a366-4c00-8e37-f180cc55fb01-e1560863231414.jpg?w=1080&ssl=1"
+                  : "https://i.udemycdn.com/user/200_H/51101684_c590_2.jpg",
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
+            );
+          },
+        ),
         AnimatedBuilder(
-            animation: _pageController,
-            builder: (context, snapshot) {
-              var value = _pageController.positions.isNotEmpty ? _pageController?.page ?? 0.0 : 0.0;
-              var countPage = 5;
-              Color color = MultiLerpColor.lerpFromCount(countPage, value: value);
-              return Container(
-                color: color,
-              );
-            }),
+          animation: _pageController,
+          builder: (context, snapshot) {
+            var value = _pageController.positions.isNotEmpty
+                ? _pageController?.page ?? 0.0
+                : 0.0;
+            var countPage = 5;
+            Color color = MultiLerpColor.lerpFromCount(countPage, value: value);
+            return Container(color: color.withOpacity(0.5));
+          },
+        ),
         Container(
           padding: EdgeInsets.only(top: 80),
           child: ScrollableContentWidget(
@@ -50,11 +78,10 @@ class _EventPageState extends State<EventPage> {
               CircleAvatar(
                 maxRadius: 40,
                 backgroundImage: CachedNetworkImageProvider(
-                    "https://eventostech.com.br/wp-content/uploads/2019/06/flutter-talks-br-eventostech-800x450.jpg"),
+                  "https://eventostech.com.br/wp-content/uploads/2019/06/flutter-talks-br-eventostech-800x450.jpg",
+                ),
               ),
-              Container(
-                height: 10,
-              ),
+              SizedBox(height: 10),
               Text(
                 "Nome do evento",
                 style: TextStyle(
@@ -62,16 +89,12 @@ class _EventPageState extends State<EventPage> {
                     fontWeight: FontWeight.bold,
                     fontSize: 18),
               ),
-              Container(
-                height: 10,
-              ),
+              SizedBox(height: 10),
               Text(
                 "12/01/1919",
                 style: TextStyle(color: Colors.white, fontSize: 14),
               ),
-              Container(
-                height: 15,
-              ),
+              SizedBox(height: 15),
               Text(
                 "djiedfjofijsofijsofijsefoisjefoisejfosifjosfijosifjoseifjsofijsoefijso ijseof jsefo isjfo eisfj osifjo sijo",
                 style: TextStyle(color: Colors.white, fontSize: 14),
